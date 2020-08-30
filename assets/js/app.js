fa7a7ea1feba9a0569e6c6467ec518564912f923
Darsh <darsh2987@gmail.com>
2020-08-31 00:15:08 +0100

    app.js created. initial js function applied for mobile navbar menu

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", function() {
+
+  function hamburger() {
+    document.querySelector("#hamburger").addEventListener("click", menu);
+
+    function menu() {
+      const nav = document.querySelector("#navbar");
+      const hamburger = document.querySelector("#hamburger");
+
+      if (nav.classList.contains("header__navbar-mobile-show")) {
+        nav.classList.remove("header__navbar-mobile-show");
+        hamburger.classList.remove("hamburger-container-open");
+      } else {
+        nav.classList.add("header__navbar-mobile-show");
+        hamburger.classList.add("hamburger-container-open");
+      }
+    }
+  }
+
+  hamburger();
+
+})
