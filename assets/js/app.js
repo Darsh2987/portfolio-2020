7900a16c551e048a47d89e8e4c784fa6bd717599
Darsh <darsh2987@gmail.com>
2020-08-31 11:27:23 +0100

    hamburger-icon - function and styling complete.

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,14 +5,14 @@ window.addEventListener("load", function() {
 
     function menu() {
       const nav = document.querySelector("#navbar");
-      const hamburger = document.querySelector("#hamburger");
+      const hamburger = document.querySelector(".hamburger");
 
       if (nav.classList.contains("header__navbar-mobile-show")) {
         nav.classList.remove("header__navbar-mobile-show");
-        hamburger.classList.remove("hamburger-container-open");
+        hamburger.classList.remove("hamburger-cross");
       } else {
         nav.classList.add("header__navbar-mobile-show");
-        hamburger.classList.add("hamburger-container-open");
+        hamburger.classList.add("hamburger-cross");
       }
     }
   }
