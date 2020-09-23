d76c11696f7b750a0af7c6aa6a6f2aff59641b74
Darsh <darsh2987@gmail.com>
2020-09-23 23:29:28 +0100

    comments added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,17 @@
 import "../styles/css/styles.css";
 
 window.addEventListener("load", () => {
+  // Global Variables
   const nav = document.querySelector("#navbar");
   const hamburgerBtn = document.querySelector(".hamburger");
 
+  // Function to remove nav overlay and set hamburger icon to normal when a nav link is clicked
   function resetNav() {
     nav.classList.remove("header__navbar-mobile-show");
     hamburgerBtn.classList.remove("hamburger-cross");
   }
 
+  // function to show nav overlay when hamburger icon is cliked
   function hamburger() {
     document.querySelector("#hamburger").addEventListener("click", () => {
       if (nav.classList.contains("header__navbar-mobile-show")) {
@@ -20,20 +23,25 @@ window.addEventListener("load", () => {
     });
   }
 
+  // function to call resetNav function and smooth scroll when a nav link is clicked
   function navLinks() {
     document.querySelector("#home-link").addEventListener("click", () => {
       resetNav();
+      document.querySelector(".hero-wrapper").scrollIntoView({ behavior: "smooth" });
     });
 
     document.querySelector("#work-link").addEventListener("click", () => {
       resetNav();
+      document.querySelector("#mywork").scrollIntoView({ behavior: "smooth" });
     });
 
     document.querySelector("#projects-link").addEventListener("click", () => {
       resetNav();
+      document.querySelector("#myprojects").scrollIntoView({ behavior: "smooth" });
     });
   }
 
+  // function to reveal mywork and projects items on scroll
   function revealOnScroll() {
     let itemsToReveal = document.querySelectorAll(".projects__grid-item");
 
@@ -46,18 +54,6 @@ window.addEventListener("load", () => {
       });
     });
   }
-
-  document.querySelector("#home-link").addEventListener("click", () => {
-    document.querySelector(".hero-wrapper").scrollIntoView({ behavior: "smooth" });
-  });
-
-  document.querySelector("#work-link").addEventListener("click", () => {
-    document.querySelector("#mywork").scrollIntoView({ behavior: "smooth" });
-  });
-
-  document.querySelector("#projects-link").addEventListener("click", () => {
-    document.querySelector("#myprojects").scrollIntoView({ behavior: "smooth" });
-  });
 
   hamburger();
   navLinks();
